c99dc1c6cfc6f0629674f215b7e36bb858ca4c11
ElenaSorokinaa <149004479+ElenaSorokinaa@users.noreply.github.com>
2023-11-13 21:21:55 +0300

    Update LotteryNumbers.java

--- src/main/java/Lottery/ListExample.java
+++ src/main/java/Lottery/ListExample.java
@@ -1,27 +1,21 @@
 package Lottery;
 
 import java.lang.reflect.Array;
-import java.util.*;
+//import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
 
-public class ListExample {
+public class LotteryNumbers {
     public static void main(String[] args) {
         ArrayList<Integer> arrayList = new ArrayList<>();
         for (int i = 0; i <= 100; i++) {
             arrayList.add(i);
         }
         Collections.shuffle(arrayList);
-        Integer[] shuffledArray = arrayList.toArray(new Integer[0]);
         System.out.println(arrayList);
 
-        System.out.println(arrayList.get(0));
-        System.out.println(arrayList.get(1));
-        System.out.println(arrayList.get(2));
-        System.out.println(arrayList.get(3));
-        System.out.println(arrayList.get(4));
-        System.out.println(arrayList.get(5));
-        System.out.println(arrayList.get(6));
-        System.out.println(arrayList.get(7));
-        System.out.println(arrayList.get(8));
-        System.out.println(arrayList.get(9));
+        for (int y = 0; y < 10; y++) {
+            System.out.println(arrayList.get(y));
+        }
     }
 }
